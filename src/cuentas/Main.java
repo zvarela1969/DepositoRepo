60a7712de0285969173447dad560b464c659bacb
Zeus <Zeus@192.168.1.143>
2024-02-09 23:07:43 +0100

    Deposito (copia de trabajo al repositorio local)

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -5,14 +5,13 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
-        operativa_cuenta();
+        operativa_cuenta(6);
         
     }
     
-    private static void operativa_cuenta() {
+    private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
-        float cantidad;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
